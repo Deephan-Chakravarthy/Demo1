b3ef729e38b70f171fb10fc5288df91cd347fbd7
Deephan-Chakravarthy <deephan221099@gmail.com>
2024-01-25 12:42:14 +0530

    Script file

---
+++ script.js
@@ -0,0 +1,10 @@
+// XML-HTTP Methods:
+// Purpose:it is used to interact with the
+// servers via API
+
+//Total There are 5 steps
+
+//Step 01: Create a XML-HTTP Object
+var request =  new XMLHttpRequest();
+//Step 02: Open a Request
+request.open("GET","https://restcountries.com/v3.1/all")
