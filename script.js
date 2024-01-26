d3801b0dd938428a3547dbe3f10ca2b6e98f7b04
Deephan-Chakravarthy <deephan221099@gmail.com>
2024-01-26 16:42:28 +0530

    2

--- script.js
+++ script.js
@@ -2,3 +2,24 @@
 var request =  new XMLHttpRequest();
 
 request.open("GET","https://restcountries.com/v3.1/all")
+
+request.send();
+
+request.onload = function(){
+var res = JSON.parse(request.response);
+console.log(res);
+// Print all 250 country common name in console
+  for(var i =0;i<res.length;i++){
+    console.log(res[i].name.common)
+}
+}
+//Print the capital name, as well as area for countries
+for(var i =0;i<res.length;i++){
+    if(res[i].capital){
+        console.log(res[i].capital[0]+" "+res[i].area)
+    }
+    else{
+        console.log("Invalid capital"+res[i].area);
+    }
+}
+}
